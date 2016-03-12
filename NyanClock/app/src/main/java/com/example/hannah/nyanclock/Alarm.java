26f6acee5454cd0caab029cd00906acb64d02a7a
William <wdionio@gmail.com>
2016-03-12 19:43:05 +0800

    Changed DB design

--- NyanClock/app/src/main/java/com/example/hannah/nyanclock/Alarm.java
+++ NyanClock/app/src/main/java/com/example/hannah/nyanclock/Alarm.java
@@ -1,5 +1,6 @@
 package com.example.hannah.nyanclock;
 
+import java.util.ArrayList;
 import java.util.Date;
 import java.sql.Timestamp;
 import java.text.SimpleDateFormat;
@@ -11,35 +12,69 @@ public class Alarm {
     public static final String TABLE_NAME = "Alarms";
     public static final String COLUMN_ID = "_id";
     public static final String COLUMN_TIME = "Time";
-    public static final String COLUMN_DAY = "Day";
+    public static final String COLUMN_SUNDAY = "Sunday";
+    public static final String COLUMN_MONDAY = "Monday";
+    public static final String COLUMN_TUESDAY = "Tuesday";
+    public static final String COLUMN_WEDNESDAY = "Wednesday";
+    public static final String COLUMN_THURSDAY = "Thursday";
+    public static final String COLUMN_FRIDAY = "Friday";
+    public static final String COLUMN_SATURDAY = "Saturday";
     public static final String COLUMN_CLOCK = "Clock";
 
     private Timestamp timeStamp;
     private int id;
-    private String day;
+    private boolean sun;
+    private boolean mon;
+
+
+
+
+
+    private boolean tues;
+    private boolean wed;
+    private boolean thurs;
+
+
+
+    private boolean fri;
+    private boolean sat;
     private String time;
     private String clock; // AM or PM
 
     public Alarm(){
     }
 
-    public Alarm(int id, String hour, String minutes, String day, String clock){
+    //Values of day array SHOULD be in order: Sunday - Saturday
+    public Alarm(int id, String hour, String minutes, boolean[] day, String clock){
         this.id = id;
-        this.day = day;
         time = hour+":"+minutes +" "+clock;
+        sun = day[0];
+        mon = day[1];
+        tues = day[2];
+        wed = day[3];
+        thurs = day[4];
+        fri = day[5];
+        sat = day[6];
 
     }
 
-
-    public Alarm(String hour, String minutes,  String day, String clock){
+    //Values of day array SHOULD be in order: Sunday - Saturday
+    public Alarm(String hour, String minutes,  boolean[] day, String clock){
         time = hour+":"+minutes +" "+clock;
-        this.day = day;
+        sun = day[0];
+        mon = day[1];
+        tues = day[2];
+        wed = day[3];
+        thurs = day[4];
+        fri = day[5];
+        sat = day[6];
     }
 
     public Timestamp getTimeStamp(){
         return timeStamp;
     }
 
+    //Converts the time String of h:mm a to a time stamp
     public void createTimeStamp(){
         SimpleDateFormat dateFormat = new SimpleDateFormat("h:mm a");
         try{
@@ -50,12 +85,60 @@ public class Alarm {
         }
     }
 
-    public void setDay(String day){
-        this.day = day;
+    public boolean isSun() {
+        return sun;
     }
 
-    public String getDay(){
-        return day;
+    public void setSun(boolean sun) {
+        this.sun = sun;
+    }
+
+    public boolean isMon() {
+        return mon;
+    }
+
+    public void setMon(boolean mon) {
+        this.mon = mon;
+    }
+
+    public boolean isTues() {
+        return tues;
+    }
+
+    public void setTues(boolean tues) {
+        this.tues = tues;
+    }
+
+    public boolean isWed() {
+        return wed;
+    }
+
+    public void setWed(boolean wed) {
+        this.wed = wed;
+    }
+
+    public boolean isThurs() {
+        return thurs;
+    }
+
+    public void setThurs(boolean thurs) {
+        this.thurs = thurs;
+    }
+
+    public boolean isFri() {
+        return fri;
+    }
+
+    public void setFri(boolean fri) {
+        this.fri = fri;
+    }
+
+    public boolean isSat() {
+        return sat;
+    }
+
+    public void setSat(boolean sat) {
+        this.sat = sat;
     }
 
     public int getId(){
