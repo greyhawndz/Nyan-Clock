a7b2cc1b67b6405189e0435a25d910299242cbdd
William <wdionio@gmail.com>
2016-03-13 22:04:23 +0800

    Added CatReceiver class and Notifications

--- NyanClock/app/src/main/java/com/example/hannah/nyanclock/CatActivity.java
+++ NyanClock/app/src/main/java/com/example/hannah/nyanclock/CatActivity.java
@@ -5,9 +5,14 @@ Feed Button and Set Alarm Button
 
 package com.example.hannah.nyanclock;
 
+import android.app.AlarmManager;
+import android.app.PendingIntent;
+import android.app.Service;
 import android.content.Intent;
+import android.os.SystemClock;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.ImageButton;
 
@@ -16,6 +21,7 @@ public class CatActivity extends AppCompatActivity {
     ImageButton ibButtonFeed, ibButtonAlarm;
     final static int REQUEST_TIME = 0;
     final static String KEY_TIME = "time";
+    final static int RECEIVER = 1;
     DatabaseOpenHelper dbHelper;
     Cat cat;
 
@@ -23,6 +29,7 @@ public class CatActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cat);
+
 
         // Variables
         ibButtonFeed = (ImageButton) findViewById(R.id.ib_ButtonFeed);
@@ -37,8 +44,7 @@ public class CatActivity extends AppCompatActivity {
                 //TODO: Increase hunger meter value
                 //TODO: Increases happy meter value
 
-                //Update cat in db
-                dbHelper.updateCat(cat);
+
             }
         });
 
@@ -54,7 +60,46 @@ public class CatActivity extends AppCompatActivity {
 
 
 
+
+
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        dbHelper.updateCat(cat);
+
     }
 
 
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        dbHelper.updateCat(cat);
+
+
+        int hungerVal = cat.getHunger();
+        int happyVal = cat.getHappiness();
+        if(happyVal < 50 || hungerVal < 50){
+
+
+            Intent broadcastIntent = new Intent(getBaseContext(), CatReceiver.class);
+            PendingIntent pendingIntent
+                    = PendingIntent.getBroadcast(getBaseContext(),
+                    RECEIVER,
+                    broadcastIntent,
+                    PendingIntent.FLAG_UPDATE_CURRENT);
+
+            ((AlarmManager)getSystemService(Service.ALARM_SERVICE))
+                    .set(AlarmManager.ELAPSED_REALTIME_WAKEUP,
+                            SystemClock.elapsedRealtime(),
+                            pendingIntent);
+        }
+
+
+
+
+    }
 }
---
+++ NyanClock/app/src/main/java/com/example/hannah/nyanclock/CatReceiver.java
@@ -0,0 +1,42 @@
+package com.example.hannah.nyanclock;
+
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.app.Service;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.support.v4.app.NotificationCompat;
+import android.util.Log;
+
+public class CatReceiver extends BroadcastReceiver {
+
+    final static int CA_PENDINGINTENT = 0;
+    final static int NOTIF_ID = 0;
+
+    public CatReceiver() {
+    }
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        // TODO: This method is called when the BroadcastReceiver is receiving
+        // an Intent broadcast.
+
+        PendingIntent pendingIntent = PendingIntent.getActivity(context,
+                CA_PENDINGINTENT,
+                new Intent(context, MainActivity.class),
+                PendingIntent.FLAG_UPDATE_CURRENT);
+
+        NotificationCompat.Builder builder
+                = new NotificationCompat.Builder(context)
+                .setTicker("Your Cat needs attention!")
+                .setContentTitle("Hungry/Sad Cat")
+                .setContentText("Play/Feed your cat to make it feel better.")
+                .setSmallIcon(R.mipmap.ic_launcher)
+                .setContentIntent(pendingIntent);
+
+        ((NotificationManager)context.getSystemService(Service.NOTIFICATION_SERVICE))
+                .notify(NOTIF_ID, builder.build());
+
+    }
+}
